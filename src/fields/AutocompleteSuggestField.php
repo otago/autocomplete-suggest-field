a052095535e95735689db178a0b9b09e32380b7f
Torleif West <torleif.west@op.ac.nz>
2018-07-17 11:37:23 +1200

    Fix - use exposed resource js

--- src/fields/AutocompleteSuggestField.php
+++ src/fields/AutocompleteSuggestField.php
@@ -37,7 +37,7 @@ class AutocompleteSuggestField extends TextField {
 	 * @throws Exception
 	 */
 	function __construct($name, $controller, $title = null, $form = null, DataObject $dataojbect = null) {
-		Requirements::javascript('autocomplete-suggest-field/javascript/AutocompleteSuggestField.js');
+		Requirements::javascript('resources/otago/autocomplete-suggest-field/javascript/AutocompleteSuggestField.js');
 
 		$this->name = $name;
 		$this->controller = $controller;
