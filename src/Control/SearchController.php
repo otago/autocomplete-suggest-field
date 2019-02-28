84a94aea8ec2c58e35d1eef7628d065301cf9190
torleif <torleifw@gmail.com>
2019-02-28 14:40:00 +1300

    Update SearchController.php

--- src/Control/SearchController.php
+++ src/Control/SearchController.php
@@ -40,12 +40,8 @@ class SearchController extends Controller {
         if (strlen($request->getVar('query')) < 1) {
             throw new HTTPResponse_Exception('Query not long enough', 400);
         }
-        if (!singleton($request->getVar('ClassName'))->hasField('Title')) {
-            throw new HTTPResponse_Exception($request->getVar('ClassName') . ' must have Title to search', 400);
-        }
-        $SearchedDataObject = Injector::inst()->create($request->getVar('ClassName'));
 
-        $dbobjects = $SearchedDataObject::get()->filter(array('Title:StartsWith:nocase' => $request->getVar('query')))->limit(10);
+        $dbobjects = $this->buildsearchquery($request);
 
         $returnarray = [];
         foreach ($dbobjects as $obj) {
@@ -53,6 +49,34 @@ class SearchController extends Controller {
         }
 
         print_r(json_encode($returnarray));
+    }
+
+    /**
+     * return a query that will return results of the search. Will look at $searchable_fields
+     * of the dataobject. If none are found, it will resort to Title. Title must exist on the dataobject
+     * 
+     * @param HTTPRequest $request
+     * @return \DataList
+     * @throws HTTPResponse_Exception
+     */
+    public function buildsearchquery(HTTPRequest $request) {
+        $SearchedDataObject = Injector::inst()->create($request->getVar('ClassName'));
+
+        $searchfields = $SearchedDataObject->config()->get('searchable_fields');
+        if ($searchfields) {
+            $searchquery = [];
+            foreach ($searchfields as $field) {
+                $searchquery[$field . ':StartsWith:nocase'] = $request->getVar('query');
+            }
+
+            return $SearchedDataObject::get()->filterAny($searchquery)->limit(10);
+        }
+
+        if (!singleton($request->getVar('ClassName'))->hasField('Title')) {
+            throw new HTTPResponse_Exception($request->getVar('ClassName') . ' must have Title to search', 400);
+        }
+
+        return $SearchedDataObject::get()->filter(array('Title:StartsWith:nocase' => $request->getVar('query')))->limit(10);
     }
 
     public function Link($action = null) {
